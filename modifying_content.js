be8398db6b97952c18fcfabe5e66ace7cc0a7c7f
Emre Gemici <35531700+emrgem@users.noreply.github.com>
2024-05-02 14:07:00 -0400

    Create modifying_content.js

---
+++ modifying_content.js
@@ -0,0 +1,22 @@
+//1 Change the text content of the "app-title" to "Updated Task List".
+
+//2 Change the background color of all items with class "item" to light blue.
+
+
+//3 Update the placeholder text of the input element to "Enter a new task".
+
+//4 Change the text color of the "Project Overview" heading to green.
+
+//5 Set the background color of the "overview" div to yellow.
+
+//6 Update the text content of the "info" div to "Tasks to be completed today".
+
+//7 Change the font size of all members in the "family" div to "18px".
+
+
+//8 Change the border color of the "family" div to red.
+
+//9 Set the text color of each "member" in the "family" div to a different color.
+
+
+//10 Update the text of the first "item" to "Task 1 - Completed".
